73762ead15f616230cc432139b77d0bd96e7c585
Al-Giovinetti <giovinettialessio@gmail.com>
2023-06-05 17:19:44 +0200

    Collegate img all' index

--- js/myscript.js
+++ js/myscript.js
@@ -3,6 +3,37 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            images : [
+                {
+                    img:"img/ace-ventura.jpg",
+                    title:"Ace Ventura",
+                    actor:"Jim Carrey"
+                },
+                {
+                    img:"img/jurassic-park.webp",
+                    title:"Jurassic Park",
+                    actor:"Sam Neill" 
+                },
+                {
+                    img:"img/matrix.webp",
+                    title:"Matrix",
+                    actor:"Keanu Reeves"
+                },
+                {
+                    img:"img/pulp-fiction.jpg",
+                    title:"Pulp Fiction",
+                    actor:"Uma Thurman" 
+                },
+                {
+                    img:"img/scream.avif",
+                    title:"Scream",
+                    actor:"Neve Campbell"
+                }
+                
+            ],
+
+            activeIndex: 0,
         }
-    }
-}).mount("#app")
+    },
+}).mount("#app");
+
